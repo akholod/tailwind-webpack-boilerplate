d7ba8da02d37b249903cde54ddc08b57eefe33d3
Andrey Kokhanevich <andrey@itsua.org>
2019-06-04 13:21:55 +0300

    purgecss plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,9 @@ module.exports = {
               plugins: [
                 require('tailwindcss'),
                 require('autoprefixer'),
+                require('@fullhuman/postcss-purgecss')({
+                  content: ['./src/**/*.html'],
+                }),
               ],
             },
           }
